c6b0d2f6c229c3e6c804a4e6f966636ebe6e3508
zzboncak <zachary.zboncak@gmail.com>
2021-09-15 10:24:21 -0500

    more logging

--- src/app.js
+++ src/app.js
@@ -34,11 +34,11 @@ app.use("/api/people", peopleRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
+  // if (NODE_ENV === "production") {
+  //   response = { error: { message: "server error" } };
+  // } else {
     response = { message: error.message, error };
-  }
+  // }
   res.status(500).json(response);
 });
 
