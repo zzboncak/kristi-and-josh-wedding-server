eebd26174622ade2381a193cbb8f858f0e7af47d
zzboncak <zachary.zboncak@gmail.com>
2021-09-15 11:20:09 -0500

    adjustments to db connection

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL
+  connection: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.set("db", db);
